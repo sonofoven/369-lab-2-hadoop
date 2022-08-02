856721ef1335a89010aadfcd0c231bdbdf7d84b3
amigler <amigler@calpoly.edu>
2022-08-02 21:39:58 +0000

    upgrade gradle to v7.5

--- src/main/java/csc369/HadoopApp.java
+++ src/main/java/csc369/HadoopApp.java
@@ -33,6 +33,11 @@ public class HadoopApp {
 	    job.setMapperClass(AccessLog.MapperImpl.class);
 	    job.setOutputKeyClass(AccessLog.OUTPUT_KEY_CLASS);
 	    job.setOutputValueClass(AccessLog.OUTPUT_VALUE_CLASS);
+        } else if ("AccessLog2".equalsIgnoreCase(otherArgs[0])) {
+	    job.setReducerClass(AccessLog2.ReducerImpl.class);
+	    job.setMapperClass(AccessLog2.MapperImpl.class);
+	    job.setOutputKeyClass(AccessLog2.OUTPUT_KEY_CLASS);
+	    job.setOutputValueClass(AccessLog2.OUTPUT_VALUE_CLASS);
 	} else {
 	    System.out.println("Unrecognized job: " + otherArgs[0]);
 	    System.exit(-1);
